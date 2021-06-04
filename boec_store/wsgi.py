e3c4d6347932a79c3c04eccc2878ae795df61f39
TLHuongw <41871821+lanhuong1999@users.noreply.github.com>
2021-06-04 23:22:13 +0700

    first commit

---
+++ boec_store/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for boec_store project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boec_store.settings')
+
+application = get_wsgi_application()
